11ca8fb91761d3c0eeae1e9e084bc87280bc8d74
adnan-mujagic <adnanmujagic@outlook.com>
2021-05-19 17:44:38 +0200

    Adding Register!

--- app/src/main/java/com/example/catjam/RegisterActivity.java
+++ app/src/main/java/com/example/catjam/RegisterActivity.java
@@ -2,13 +2,36 @@ package com.example.catjam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
-
+    
+    EditText username;
+    EditText email;
+    EditText password;
+    EditText repeatPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        username = findViewById(R.id.register_username);
+        email = findViewById(R.id.register_email);
+        password = findViewById(R.id.register_password);
+        repeatPassword = findViewById(R.id.register_repeat_password);
+    }
+    
+    public void register(View view){
+        String inputPassword = password.getText().toString();
+        String inputRepeatPassword = repeatPassword.getText().toString();
+        
+        if(inputPassword.equals(inputRepeatPassword)){
+            Intent intent = new Intent(this,MainActivity.class);
+            startActivity(intent);
+            Toast.makeText(this, "You have successfully registered!", Toast.LENGTH_SHORT).show();
+        }
     }
 }
